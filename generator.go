7292faa14fcedfef393da01b577be1d509907810
Matheus de Barros Silva <mdebarros@onestock-retail.com>
2022-06-03 15:34:04 +0200

    better performance for generate doc

--- generator.go
+++ generator.go
@@ -9,18 +9,6 @@ import (
 	"github.com/swaggest/openapi-go/openapi3"
 )
 
-func keepImportsOnSave() openapi3.Operation {
-	ioutil.WriteFile("void", []byte{}, 0644)
-	fmt.Errorf("")
-	op := openapi3.Operation{}
-	return op
-}
-
-type ApiDocument struct {
-	Title   string
-	Version string
-}
-
 func GenerateDoc(r *Router) error {
 	// Read the Router.routes ang get  "name", "method", "query", "path", "body".
 	reflector := openapi3.Reflector{}
@@ -28,35 +16,16 @@ func GenerateDoc(r *Router) error {
 
 	s := reflector.SpecEns() //  Création de la Spec (Object contenant les informations du service)
 
+	// o(nRoutes*(nParams+nResps))
 	for _, op := range r.routes {
 		if &op != nil {
-
 			oasOp := openapi3.Operation{}
 
-			if strings.Contains(op.path, "{") {
-				_path := op.path
-				//var params []*openapi3.Parameter
-
-				for strings.Contains(_path, "{") {
-					_, aft, _ := strings.Cut(_path, "{")
-					_path = aft
-					pName, _, _ := strings.Cut(aft, "}")
-
-					oasOp.Parameters = append(oasOp.Parameters,
-
-						openapi3.ParameterOrRef{
-							Parameter: &openapi3.Parameter{
-								Name: pName,
-								In:   openapi3.ParameterInPath,
-							},
-						},
-					)
-				}
-
+			if &op.path != nil && strings.Contains(op.path, "{") { //o(len(op.path))
+				op.setParameters(&oasOp)
 			}
-			// No need to "convert" the op.method on http.Method<type> as http.Method is nothing more than a string like "PATH", "GET", etc
-			// BUT probably need to be uppercase
-			reflector.SetRequest(&oasOp, op.input, strings.ToUpper(op.method)) // op = GET avec OrderItemRequestParams tant qu'input
+
+			reflector.SetRequest(&oasOp, op.input, strings.ToUpper(op.method))
 
 			if op.resps != nil {
 				for code, body := range op.resps {
@@ -64,8 +33,14 @@ func GenerateDoc(r *Router) error {
 				}
 			}
 
-			s.AddOperation(op.method, op.path, oasOp)
+			if op.internal {
+				oasOp.MapOfAnything = map[string]interface{}{
+					"x-internal": true,
+				}
+			}
+			oasOp.Deprecated = &op.deprecated
 
+			s.AddOperation(op.method, op.path, oasOp)
 		}
 	}
 
@@ -73,7 +48,35 @@ func GenerateDoc(r *Router) error {
 	if err == nil {
 		fmt.Print("NO ERROR")
 	}
-	ioutil.WriteFile("openapiROUTER.json", b, 0644) // écrire la doc résultat dans un file
+	ioutil.WriteFile("openapiHandler.json", b, 0644) // écrire la doc résultat dans un file
 
 	return nil
+}
+
+// o(nParams)
+func (r *Route) setParameters(oasOp *openapi3.Operation) {
+	_path := r.path
+	n := strings.Count(_path, "{") //o(len(_path))
+
+	oasOp.Parameters = make([]openapi3.ParameterOrRef, n)
+
+	//o(n + 2*strings.Cut)
+	for n >= 0 {
+		n--
+
+		// Get param name between '{' et '}'
+		_, aft, _ := strings.Cut(_path, "{")
+		_path = aft
+		pName, _, ok := strings.Cut(aft, "}")
+
+		if ok {
+			// o(1)
+			oasOp.Parameters[n] = openapi3.ParameterOrRef{
+				Parameter: &openapi3.Parameter{
+					Name: pName,
+					In:   openapi3.ParameterInPath,
+				},
+			}
+		}
+	}
 }
