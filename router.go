7e442f7a82e70085e2aed603864fad18ea74977d
Matheus de Barros Silva <mdebarros@onestock-retail.com>
2022-06-03 15:34:34 +0200

    add properties to Route

--- router.go
+++ router.go
@@ -12,33 +12,39 @@ func (HTTPHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
 
 }
 
+type Info struct {
+	name    string
+	version string //...
+}
+
 type Router struct {
 	routes       []Route
-	nbRoutes     int
+	baseUrl      string
 	defaultResps map[int]interface{}
 }
 
 type Route struct {
-	path    string
-	method  string
-	input   interface{}         // No need to map by method because of the line above ^^
-	resps   map[int]interface{} // outputs per return code
-	handler HTTPHandler
+	path       string
+	method     string
+	input      interface{}         // No need to map by method because of the line above ^^
+	resps      map[int]interface{} // outputs per return code
+	handler    HTTPHandler
+	internal   bool
+	deprecated bool
 }
 
 func InitApi(name, version string) *Router {
 	r := new(Router)
 	r.routes = make([]Route, 30)
-	r.nbRoutes = 0
 
 	return r
 }
 
 func NewRouter() *Router {
 	r := new(Router)
-	r.nbRoutes = 0
 	return r
 }
+
 func (r *Router) NewRoute(path, meth string) *Router {
 
 	r.routes = append(r.routes, Route{
@@ -53,7 +59,7 @@ func CreateHTTPHandler(h HTTPHandler, input interface{}, out200 interface{}) {
 	// Among other stuff, return the function like we already do, but this time, calling the handler function passing the parameters needed
 }
 
-// Router as param or as caller ? -> Param for keeping the calling pattern
+// Router as param or as caller ? -> Param to keep the function calling pattern
 // Pass "app" as parameter -> Get the real router and add it to the handlers
 func RegisterRoutes(r *Router) (*mux.Router, error) {
 	router := new(mux.Router)
