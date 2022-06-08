3c8a61db0c8257ab9b29ebb3ab12de7a22519eca
Matheus de Barros Silva <mdebarros@onestock-retail.com>
2022-06-08 14:59:12 +0200

    handler to http.HandlerFunc

--- router.go
+++ router.go
@@ -9,12 +9,11 @@ import (
 type HTTPHandler func(w http.ResponseWriter, r *http.Request) // TODO :  add di.Container
 
 func (HTTPHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
-
 }
 
 type Info struct {
 	Name    string
-	Version string //...
+	Version string
 }
 
 type Router struct {
@@ -28,7 +27,7 @@ type Route struct {
 	Method     string
 	Input      interface{}         // No need to map by method because of the line above ^^
 	Resps      map[int]interface{} // outputs per return code
-	Handler    HTTPHandler
+	Handler    http.HandlerFunc
 	Internal   bool
 	Deprecated bool
 }
@@ -46,7 +45,6 @@ func NewRouter() *Router {
 }
 
 func (r *Router) NewRoute(path, meth string) *Router {
-
 	r.Routes = append(r.Routes, Route{
 		Path:   path,
 		Method: meth,
@@ -66,10 +64,28 @@ func RegisterRoutes(r *Router) (*mux.Router, error) {
 
 	for _, route := range r.Routes {
 		if &route != nil && &route.Path != nil && &route.Method != nil {
-			router.NewRoute().Path(route.Path).Methods(route.Method).Handler(route.Handler) //Add handler
+			if &route.Handler != nil {
+				router.NewRoute().Path(route.Path).Methods(route.Method).Handler(route.Handler) //Add handler
+			}
 		}
 		// Shall we add some error handling here? If an error, then log (the x route couldn't be set and continue ?)
+		// Or even the route doesn't contain the path or the method or the handler ...
 	}
 
 	return router, nil
+}
+
+// Router as param or as caller ? -> Param to keep the function calling pattern
+// Pass "app" as parameter -> Get the real router and add it to the handlers
+func AttachRoutes(r *Router, muxRouter *mux.Router) (*mux.Router, error) {
+	for _, route := range r.Routes {
+		if &route != nil && &route.Path != nil && &route.Method != nil {
+			if &route.Handler != nil {
+				muxRouter.NewRoute().Path(route.Path).Methods(route.Method).Handler(route.Handler) //Add handler
+			}
+		}
+		// Shall we add some error handling here? If an error, then log (the x route couldn't be set and continue ?)
+		// Or even the route doesn't contain the path or the method or the handler ...
+	}
+	return muxRouter, nil
 }
